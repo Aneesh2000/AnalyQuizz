5b3a333d7689d52e5b4e8831f0d23a043cf288b5
Aneesh <aneeshgarray2000@gmail.com>
2025-06-25 19:07:45 -0700

    Expose FastAPI app directly for Vercel runtime

--- api/index.py
+++ api/index.py
@@ -1,7 +1,1 @@
-from mangum import Mangum
-
-# Import your FastAPI application instance
-from main import app as fastapi_app
-
-# Create handler for Vercel serverless function
-handler = Mangum(fastapi_app, lifespan="off") 
+from main import app  # Vercel will detect this ASGI application 
